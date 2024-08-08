ef08fb4519973e65d94d17eb9eb6cd68bc7f82ed
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-08-07 22:35:36 -0400

    chore rename index file extension

--- src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-// Reexport your entry components here
-import Carousel from './components/carousel.svelte';
-import CarouselItem from './components/carousel-item.svelte';
-import CarouselButton from './components/carousel-button.svelte';
-
-export { Carousel, CarouselItem, CarouselButton };
