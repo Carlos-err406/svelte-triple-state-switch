34903d369e13a4de173d58ec9e3fca7d189f2979
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-08-07 23:20:35 -0400

    using new imports in playground

--- playground/src/routes/+page.svelte
+++ playground/src/routes/+page.svelte
@@ -1,27 +1,21 @@
 <script lang="ts">
-	import { Carousel, CarouselButton, CarouselItem } from 'svelte-inplace-carousel';
+	import * as Carousel from 'svelte-inplace-carousel';
 </script>
 
-<Carousel>
-	<svelte:fragment let:kind slot="prev">
-		<CarouselButton {kind} class="scale-150">◀️</CarouselButton>
-	</svelte:fragment>
-	<svelte:fragment let:kind slot="next">
-		<CarouselButton {kind} class="scale-150">▶️</CarouselButton>
-	</svelte:fragment>
-	<CarouselItem>
+<Carousel.Root>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-red-500">A</div>
-	</CarouselItem>
-	<CarouselItem>
+	</Carousel.Item>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-blue-500">B</div>
-	</CarouselItem>
-	<CarouselItem>
+	</Carousel.Item>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-yellow-500">C</div>
-	</CarouselItem>
-	<CarouselItem>
+	</Carousel.Item>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-pink-500">D</div>
-	</CarouselItem>
-	<CarouselItem>
+	</Carousel.Item>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-green-500">E</div>
-	</CarouselItem>
-</Carousel>
+	</Carousel.Item>
+</Carousel.Root>
