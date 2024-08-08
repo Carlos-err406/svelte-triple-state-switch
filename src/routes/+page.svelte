9b708c832f7022ef9037ef6936898b50fa9366fe
Carlos Daniel Vilaseca <carlosd.vilaseca@avangenio.com>
2024-08-07 23:10:47 -0400

    rf: export like shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,51 @@
 <script lang="ts">
-	import { Carousel, CarouselButton, CarouselItem } from '$lib/index.js';
+	import * as Carousel from '$lib/index.js';
 </script>
 
-<Carousel>
-	<svelte:fragment let:kind slot="prev">
-		<CarouselButton {kind} class="scale-150">◀️</CarouselButton>
+<Carousel.Root class="[&_.carousel-item]:rounded-full [&_.carousel-item]:overflow-clip">
+	<svelte:fragment let:builder slot="prev">
+		<Carousel.Button {...builder} class="scale-150">◀️</Carousel.Button>
 	</svelte:fragment>
-	<svelte:fragment let:kind slot="next">
-		<CarouselButton {kind} class="scale-150">▶️</CarouselButton>
+	<svelte:fragment let:builder slot="next">
+		<Carousel.Button {...builder} class="scale-150">▶️</Carousel.Button>
 	</svelte:fragment>
-	<CarouselItem>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-red-500">A</div>
-	</CarouselItem>
-	<CarouselItem>
+	</Carousel.Item>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-blue-500">B</div>
-	</CarouselItem>
-	<CarouselItem>
+	</Carousel.Item>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-yellow-500">C</div>
-	</CarouselItem>
-	<CarouselItem>
+	</Carousel.Item>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-pink-500">D</div>
-	</CarouselItem>
-	<CarouselItem>
+	</Carousel.Item>
+	<Carousel.Item>
 		<div class="size-60 flex items-center justify-center bg-green-500">E</div>
-	</CarouselItem>
-</Carousel>
+	</Carousel.Item>
+</Carousel.Root>
+
+<Carousel.Root>
+	<svelte:fragment let:builder slot="prev">
+		<Carousel.Button {...builder} class="scale-150">◀️</Carousel.Button>
+	</svelte:fragment>
+	<svelte:fragment let:builder slot="next">
+		<Carousel.Button {...builder} class="scale-150">▶️</Carousel.Button>
+	</svelte:fragment>
+	<Carousel.Item>
+		<div class="size-60 flex items-center justify-center bg-red-500">A</div>
+	</Carousel.Item>
+	<Carousel.Item>
+		<div class="size-60 flex items-center justify-center bg-blue-500">B</div>
+	</Carousel.Item>
+	<Carousel.Item>
+		<div class="size-60 flex items-center justify-center bg-yellow-500">C</div>
+	</Carousel.Item>
+	<Carousel.Item>
+		<div class="size-60 flex items-center justify-center bg-pink-500">D</div>
+	</Carousel.Item>
+	<Carousel.Item>
+		<div class="size-60 flex items-center justify-center bg-green-500">E</div>
+	</Carousel.Item>
+</Carousel.Root>
